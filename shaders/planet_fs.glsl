6f8c1172d45afd0a30dd5c1ffc2697d9a9f94b34
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-04-15 22:58:18 +0200

    Add simple simplex noise planet

--- shaders/planet_fs.glsl
+++ shaders/planet_fs.glsl
@@ -2,6 +2,7 @@ precision mediump float;
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vEcPosition;
+varying vec3 vWPos; // world position
 
 uniform vec4 mainColor;
 uniform float specularExponent;
@@ -10,6 +11,8 @@ uniform sampler2D mainTexture;
 
 #pragma include "light.glsl"
 #pragma include "shadowmap.glsl"
+#pragma include "noise3D.glsl"
+
 
 void main(void)
 {
@@ -29,4 +32,9 @@ void main(void)
     vec3 color = max((diffuse+diffusePoint)*visibility,_ambient.xyz)*mainColor.xyz;
 
     gl_FragColor = vec4(texture2D(mainTexture,vUv).xyz*color.xyz, 1.0)+vec4((specular+specularPoint)*specularColor.xyz,0.0);
+    float noise = snoise(vWPos*3.0)+
+        snoise(vWPos*1.0)+
+        snoise(vWPos*0.5)
+        ; // simple noise
+    gl_FragColor = vec4(noise ,noise ,noise ,1.0);
 }
