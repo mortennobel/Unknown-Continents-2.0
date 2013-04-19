aa7373d70ca9d7ffd3f4251e626d254194608b8b
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-04-19 18:56:26 +0200

    RequireJS error handling

--- js/unknown-continents-2.0.js
+++ js/unknown-continents-2.0.js
@@ -15,5 +15,17 @@ requirejs(['kick', 'uc2/SceneFactory'],
             enableDebugContext: debug
         });
         SceneFactory();
+    },
+    function(err){
+        var domElement = document.getElementById('canvas');
+        domElement.innerHTML = "";
+        domElement.width = window.innerWidth;
+        domElement.height = window.innerHeight - domElement.offsetTop;
+        errorMessage = document.createElement("div");
+        errorMessage.style.cssText = domElement.style.cssText + ";width:" + domElement.width + "px;height:" + domElement.height +
+            "px;display: table-cell;vertical-align: middle;background:#ffeeee;";
+        errorMessage.innerHTML = "<div style='padding:12px;text-align: center;'>Error loading WebGL application</div>";
+        domElement.parentNode.replaceChild(errorMessage, domElement);
+        console.log(err);
     }
 );
