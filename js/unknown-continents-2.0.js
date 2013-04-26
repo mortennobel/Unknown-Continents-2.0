db5186fbbcd063f24ffdaa3b2758d795321d2a62
Emil <kastbjerg@gmail.com>
2013-04-26 12:34:28 +0200

    dat.gui added

--- js/unknown-continents-2.0.js
+++ js/unknown-continents-2.0.js
@@ -12,7 +12,7 @@ requirejs.config({
 
 requirejs(['kick', 'uc2/SceneFactory'],
     function (kick,SceneFactory) {
-        var engine = new kick.core.Engine('canvas', {
+        new kick.core.Engine('canvas', {
             enableDebugContext: debug
         });
         SceneFactory();
