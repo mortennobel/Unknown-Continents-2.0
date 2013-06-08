a8c747b07f1178f7cec7e29946bf7a5b861d9ee5
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-06-08 23:26:05 +0200

    Add more details to simplex noise

--- shaders/simplex_noise_fs.glsl
+++ shaders/simplex_noise_fs.glsl
@@ -5,7 +5,7 @@ varying vec2 uv;
 
 uniform float scale;
 
-#pragma include "noise3D.glsl"
+#pragma include "noise4D.glsl"
 
 vec3 unproject(vec2 uv){
     const float PI = 3.14159265359;
@@ -21,6 +21,12 @@ void main(void)
 {
     vec3 pos = unproject(uv);
 
-    float n = snoise(pos.xyz*scale);
-	gl_FragColor = vec4(n);
+    float n =
+        snoise(vec4(pos.xyz*scale,0.0))+
+        0.5*snoise(vec4(pos.xyz*scale*2.0,2.0))+
+        0.25*snoise(vec4(pos.xyz*scale*4.0,4.0))+
+        0.125*snoise(vec4(pos.xyz*scale*8.0,6.0))+
+        0.0625*snoise(vec4(pos.xyz*scale*16.0,8.0))
+        ;
+	gl_FragColor = vec4(0.4*n+0.5);
 }
