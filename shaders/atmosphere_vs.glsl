e14c4fdb5159229e947419d7b47701f946cb3ed6
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-06-10 22:06:23 +0200

    Improve atmosphere

--- shaders/atmosphere_vs.glsl
+++ shaders/atmosphere_vs.glsl
@@ -10,8 +10,27 @@ varying vec3 vNormal;
 varying vec3 vEcPosition;
 varying float vDistToOrigin;
 
+mat4 rotationMatrix(vec3 axis_, float angle)
+{
+    vec3 axis = normalize(axis_);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+
 void main(void) {
+    vec3 origin = (_mv * vec4(0.0,0.0,0.0,1.0)).xyz;
+    float lengthToOrigin = length(origin);
+    float atmosphereRotationAngle = 3.1415*0.5 - acos(1.0/lengthToOrigin);
+
     vec4 v = vec4(vertex, 1.0);
+    mat4 rotation = rotationMatrix(cross(normalize((_mv * v).xyz),normalize(vertex)), atmosphereRotationAngle);
+    v = rotation * v;
     // compute position
     gl_Position = _mvProj * v;
 
