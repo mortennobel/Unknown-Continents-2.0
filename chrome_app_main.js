6179f8b34c6278284c9d6d71cfc2604e6c793db7
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-06-21 23:08:00 +0200

    Add google chrome packaged app

--- chrome_app_main.js
+++ chrome_app_main.js
@@ -1,17 +1,14 @@
 /**
+ * Example usage of offline
+ * OSX
+ * open -a "Google Chrome Canary" --args --load-and-launch-app=/path/to/files
+ *
  * Listens for the app launching then creates the window
  *
  * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-  // Center window on screen.
-  var screenWidth = screen.availWidth;
-  var screenHeight = screen.availHeight;
-  var width = 500;
-  var height = 300;
-
-
   chrome.app.window.create('index.html', {
     frame: "none",
     'width': 1024,
